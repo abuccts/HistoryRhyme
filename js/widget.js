e354986f9387052b8b30e50ab4f0fe003b9a7949
abuccts <abuccts@gmail.com>
2016-04-30 00:11:32 +0800

    sync

--- js/widget.js
+++ js/widget.js
@@ -4,26 +4,42 @@ var commonUrls =
 };
 
 
-function form(rootEle, d) {
-	var fields = ['object', 'scene', 'period', 'person', 'verb'];
+var tagColors = ['red', 'orange', 'yellow', 'olive', 'green', 'teal', 'blue', 'violet', 'purple', 'pink', 'brown', 'grey'];
+
+
+function mkform(rootEle, d, fields, processFunc) {
 	$.each(fields, function(i, field) {
 		$fieldDiv = $('<div class="field"></div>');
 		$fieldDiv.append('<label>' + field + '</label>');
-		if (i < 3) {
-			$fieldInput = s;
-		} else if (i == 3) {
-			;
+		if (i < 4) {
+			if (i < 3) {
+				$fieldInput = $('<div id="' + field + 'Input" name="' + field + '" class="ui fluid selection dropdown"></div>');
+			} else {
+				$fieldInput = $('<div id="' + field + 'Input" name="' + field + '" class="ui fluid search selection dropdown"></div>');
+			}
+			$fieldInput.append('<input type="hidden" name="' + field + '"><i class="dropdown icon"></i>' + 
+								'<div class="default text">' + field + '</div>');
+			$menu = $('<div class="menu"></div>');
+			$.each(d[field], function(j, item) {
+				$menu.append('<div class="item" data-value="' + item[0] + '">' + item[1] + '</div>');
+			});
+			$fieldInput.append($menu);
 		} else {
-			;
+			$fieldInput = $('<input id="' + field + 'Input" name="' + field + '" type="text" placeholder="' + field + '">');
 		}
+		$fieldDiv.append($fieldInput);
+		rootEle.append($fieldDiv);
 	});
+	$('.ui.dropdown').dropdown();
+	processFunc();
 }
 
-function table(rootEle, d, clickFunc) {
+
+function mktable(rootEle, d, clickFunc) {
 	$.each(d, function(i, field) {
 		$trow = $('<tr></tr>');
 		$.each(field, function(j, item) {
-			$trow.append('<td>' + item + '</td>');
+			$trow.append('<td>' + item[1] + '</td>');
 		});
 		$trow.click(function() {
 			clickFunc(field);
@@ -31,4 +47,105 @@ function table(rootEle, d, clickFunc) {
 		rootEle.append($trow);
 	});
 }
+
+
+function mklist(rootEle, d, param) {
+	rootEle.html('');
+	startPos = param[0] * param[1];
+	endPos = (param[0] + 1) * param[1];
+	$.each(d.slice(startPos, endPos), function(i, eventItem) {
+		$itemDiv = $('<div class="item list-group-item"></div>');
+		$tags = $('<div class="right floated content"></div>');
+		$.each(eventItem['tag'], function(j, tagItem) {
+			var color = tagColors[tagItem.length % tagColors.length];
+			$tags.append('<div class="ui ' + color + ' label">' + tagItem + '</div>');
+		});
+		$itemDiv.append($tags);
+		$itemDiv.append('<a href="event.html?eid=' + eventItem['eid'] + '" class="list-link" target="_blank">&nbsp;</a>' +
+						'<div class="content event-content">' + eventItem['text'] + '</div>');
+		rootEle.append($itemDiv);
+	});
+}
+
+
+function mktags(rootEle, d) {
+	rootEle[0].html('');
+	rootEle[1].text(d['text']);
+	$.each(d['tag'], function(i, t) {
+		$eachTag = $('<div class="ui checkbox"></div>');
+		$eachTag.append('<input type="checkbox" name="tag">');
+		$eachTag.append('<label><a class="ui ' + tagColors[t.length % tagColors.length] + ' small label">' + t + '</a></label>');
+		$eachTag.checkbox('attach events', '.toggle.button');
+		rootEle[0].append($('<div class="field"></div>').append($eachTag));
+	});
+}
+
+
+function Slide(rootEle, d, itemDisplayNum, processFunc) {
+	rootId = rootEle.attr('id');
+	rootEle.append('<ul id="slide"></ul>');
+	$.each(d, function(i, field) {
+		rootEle.children('ul').append(processFunc(field));
+	});
+	var autoplaySlider = $('#slide').lightSlider({
+		item: itemDisplayNum,
+		easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+		auto: true,
+		loop: true,
+		pauseOnHover: true,
+		responsive: [{
+			breakpoint: 800,
+			settings: {
+				item: 3,
+				slideMove: 1,
+				slideMargin: 6
+			}
+		},
+		{
+			breakpoint:480,
+			settings: {
+				item: 2,
+				slideMove: 1
+			}
+		}]
+	});
+}
+
+
+function ExtendedCarousel(rootEle, d, cols, param) {
+	$.each(d, function(i, field) {
+		var lightSliderId = 'lightSlider' + param + i;
+		rootEle.append('<div class="cascade-item"><h3 align="center"></h3><ul id=' + lightSliderId + '></ul></div>');
+		$.each(field, function(j, img) {
+			if ( !j && img.indexOf('jpg') == -1 ) {
+				$('#' + lightSliderId).prev().html('<a class="ui ' + tagColors[img.length % tagColors.length] +
+													' large label">' + img + '</a>');
+			} else {
+				$('#' + lightSliderId).append('<li data-thumb="' + commonUrls.imgRootUrl + img +
+											'"><img src="' + commonUrls.imgRootUrl + img + '"></li>');
+			}
+		});
+	});
+
+	rootEle.imagesLoaded(function() {
+		$.each(d, function(i, field) {
+			$('#lightSlider' + param + i).lightSlider({
+				gallery: true,
+				item: 1,
+				loop: true,
+				slideMargin: 0,
+				thumbItem: 3
+			});
+		});
+		rootEle.masonry({
+			itemSelector: '.cascade-item',
+			columnWidth: function(containerWidth) {
+				return containerWidth / cols;
+			}
+		});
+        
+		$('.facet-info').resize();
+	});
+}
+
 
